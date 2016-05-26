3177f5246ae7fbf923216888afc7c903022acce6
UVADave <dpg3k@virginia.edu>
2016-05-26 12:07:30 -0400

    Add environment driven startup options

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,1 +1,31 @@
-bin/entity-id-ws
+# set blank options variables
+EZIDURL_OPT=""
+EZIDUSER_OPT=""
+EZIDPASSWD_OPT=""
+TOKENURL_OPT=""
+
+# EZID endpoint URL
+if [ -n "$EZID_URL" ]; then
+   EZIDURL_OPT="--ezidurl $EZID_URL"
+fi
+
+# EZID user name
+if [ -n "$EZID_USER" ]; then
+   EZIDUSER_OPT="--eziduser $EZID_USER"
+fi
+
+# EZID password
+if [ -n "$EZID_PASSWD" ]; then
+   EZIDPASSWD_OPT="--ezidpassword $EZID_PASSWD"
+fi
+
+# token authentication service URL
+if [ -n "$TOKENAUTH_URL" ]; then
+   TOKENURL_OPT="--tokenauth $TOKENAUTH_URL"
+fi
+
+bin/entity-id-ws $EZIDURL_OPT $EZIDUSER_OPT $EZIDPASSWD_OPT $TOKENURL_OPT
+
+#
+# end of file
+#
