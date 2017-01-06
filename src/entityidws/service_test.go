00772de22f69967c0684c1224620c24071ec65fb
UVADave <dpg3k@virginia.edu>
2017-01-06 13:47:47 -0500

    Add runtime endpoint

--- src/entityidws/service_test.go
+++ src/entityidws/service_test.go
@@ -56,6 +56,29 @@ func TestVersionCheck( t *testing.T ) {
 }
 
 //
+// runtime tests
+//
+
+func TestRuntimeCheck( t *testing.T ) {
+    expected := http.StatusOK
+    status, runtime := client.RuntimeCheck( cfg.Endpoint )
+    if status != expected {
+        t.Fatalf( "Expected %v, got %v\n", expected, status )
+    }
+
+    if runtime == nil {
+        t.Fatalf( "Expected non-nil runtime info\n" )
+    }
+
+    if runtime.AllocatedMemory == 0 ||
+            runtime.CpuCount == 0 ||
+            runtime.GoRoutineCount == 0 ||
+            runtime.ObjectCount == 0 {
+        t.Fatalf( "Expected non-zero value in runtime info but one is zero\n" )
+    }
+}
+
+//
 // statistics tests
 //
 
